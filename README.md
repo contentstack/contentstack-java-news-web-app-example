e127be73d127571bbcbbb826b07e21e5ef03323a
mshaileshr@gmail.com <Yes@383840>
2020-08-26 14:20:57 +0530

    initial commit

---
+++ README.md
@@ -0,0 +1,91 @@
+# Web-App Using Contentstack-java SDK and Spring-Boot
+
+A Sample Example Code for contentstack-Java SDK using Spring-Boot,
+This guide is meant to give you a quick taste of integration of Contentstack-Java SDK with Spring Boot web-app. If you want to create your own Spring Boot-based project, visit [Spring Initializr](https://start.spring.io/), fill in your project details, pick your options, and download a bundled up project as a zip file.
+
+
+## Step: 1 You will build a simple news web application with Spring Boot and contentstack Java SDK.
+
+
+- A favorite text editor or IDE
+
+- JDK 1.8 or later
+
+- Gradle 4+ or Maven 3.2+
+
+
+## Step: 2 How to complete this guide
+
+- To start from scratch, move on to Starting with [Spring Initializr](https://start.spring.io/).
+
+- To skip the basics, do the following:
+
+- Download and unzip the source repository for this guide, or clone it using Git: git clone https://github.com/ishaileshmishra/contentstack-java-spring-example.git
+
+- cd into contentstack-java-spring-example/initial
+
+- Jump ahead to Create a Simple Web Application.
+
+- When you finish, you can check your results against the code in gs-spring-boot/complete.
+
+## Step: 3 Add Contentstack Dependency
+
+add dependency:
+
+For Maven
+
+```
+<dependency>
+   <groupId>com.contentstack.sdk</groupId>
+   <artifactId>java</artifactId>
+   <version>1.5.3</version>
+</dependency>
+```
+
+For Gradle
+
+```
+implementation 'com.contentstack.sdk:java:1.5.3'
+```
+
+
+## Step: 4 For View Rendering
+
+```
+<dependency>
+    <groupId>org.springframework.boot</groupId>
+    <artifactId>spring-boot-starter-thymeleaf</artifactId>
+</dependency>
+```
+
+## Step: 5 Run the Application
+
+
+To run the application, run the following command in a terminal window (in the complete) directory
+
+```
+./gradlew bootRun
+```
+
+If you use Maven, run the following command in a terminal window (in the complete) directory:
+
+```
+./mvnw spring-boot:run
+```
+
+
+Now run the service with curl (in a separate terminal window), by running the following command (shown with its output):
+
+```
+$ curl localhost:8080
+Headlines from Spring Boot!
+```
+
+Or simply click on the below url hosted on herokuapp
+
+[View Website](https://shaileshmishra.herokuapp.com)
+
+
+![Screenshot](https://github.com/ishaileshmishra/example/blob/master/snapshot.png?raw=true)
+
+
---
+++ src/main/java/io/contentstack/webapp/Application.java
@@ -0,0 +1,220 @@
+package io.contentstack.webapp;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import org.json.JSONObject;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+
+import com.contentstack.sdk.ContentType;
+import com.contentstack.sdk.Contentstack;
+import com.contentstack.sdk.Entry;
+import com.contentstack.sdk.Error;
+import com.contentstack.sdk.Query;
+import com.contentstack.sdk.QueryResult;
+import com.contentstack.sdk.QueryResultsCallBack;
+import com.contentstack.sdk.ResponseType;
+import com.contentstack.sdk.Stack;
+
+@SpringBootApplication
+@Controller
+public class Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Application.class, args);
+	}
+
+
+
+	@GetMapping("/about")
+	public String about(@RequestParam(name = "name", required = false, defaultValue = "World") String name, Model model) {
+
+		String aboutUs = getAboutUs();
+		if (aboutUs == null) {
+			model.addAttribute("about", "Could not fetch AboutUs..");
+		} else {
+			model.addAttribute("about", aboutUs);
+		}
+		return "about";
+	}
+
+
+
+	@GetMapping("/contact")
+	public String contact(@RequestParam(name = "name", required = false, defaultValue = "World") String name, Model model) {
+
+		String contactUs = getContactUs();
+		if (contactUs == null) {
+			model.addAttribute("contact", "Could not fetch contactUs..");
+		} else {
+			model.addAttribute("contact", contactUs);
+		}
+		return "contact";
+	}
+
+
+
+	@GetMapping("/")
+	public String home(@RequestParam(name = "name", required = false, defaultValue = "World") String name, Model model) {
+
+		ArrayList<NewsModel> listOfHeadlines = getNewsHeadlines();
+		if (listOfHeadlines == null || listOfHeadlines.size() == 0) {
+			model.addAttribute("headlines", "Could not fetch Headlines..");
+		} else {
+			model.addAttribute("headlines", listOfHeadlines);
+		}
+		return "headline";
+	}
+
+
+	@GetMapping("/headline")
+	public String headline(@RequestParam(name = "name", required = false, defaultValue = "World") String name, Model model) {
+
+		ArrayList<NewsModel> listOfHeadlines = getNewsHeadlines();
+		if (listOfHeadlines == null || listOfHeadlines.size() == 0) {
+			model.addAttribute("headlines", "Could not fetch Headlines..");
+		} else {
+			model.addAttribute("headlines", listOfHeadlines);
+		}
+		return "headline";
+	}
+
+
+
+
+	@GetMapping("/products")
+	public String allProducts(@RequestParam(name = "name", required = false, defaultValue = "World") String name, Model model) {
+
+		ArrayList<NewsModel> listOfProducts = getAllProducts();
+		if (listOfProducts == null || listOfProducts.size() == 0) {
+			model.addAttribute("products", "Could not fetch Products..");
+		} else {
+			model.addAttribute("products", listOfProducts);
+		}
+		System.out.println("products: "+listOfProducts);
+		return "products";
+	}
+
+
+	private ArrayList<NewsModel> getNewsHeadlines() {
+
+		try {
+			ArrayList<NewsModel> newsHeadlines = new ArrayList<>();
+			final Stack stack = Contentstack.stack("blt7979d15c28261b93", "cs17465ae5683299db9d259cb6", "production");
+			ContentType contentType = stack.contentType("news");
+			Query query = contentType.query();
+			query.find(new QueryResultsCallBack() {
+				@Override
+				public void onCompletion(ResponseType responseType, QueryResult queryresult, com.contentstack.sdk.Error error) {
+					if (error == null) {
+						List<Entry> result = queryresult.getResultObjects();
+						for (Entry entry : result) {
+							JSONObject response = entry.toJSON();
+							JSONObject url = response.optJSONObject("thumbnail");
+							String imageUrl = url.optString("url");
+							newsHeadlines.add(new NewsModel(response.optString("title"),
+									response.optString("body"), imageUrl));
+						}
+					}
+				}
+			});
+			return newsHeadlines;
+		} catch (Exception e) {
+			System.out.println(e.getMessage());
+			e.printStackTrace();
+		}
+
+		return null;
+	}
+
+
+	private ArrayList<NewsModel> getAllProducts() {
+
+		try {
+			ArrayList<NewsModel> allProducts = new ArrayList<>();
+			Stack stack = Contentstack.stack("blt7979d15c28261b93", "cs17465ae5683299db9d259cb6", "production");
+			Query query = stack.contentType("products").query();
+			query.includeContentType();
+			query.find(new QueryResultsCallBack() {
+
+				@Override
+				public void onCompletion(ResponseType responseType, QueryResult queryresult, Error error) {
+					if (error == null) {
+						List<Entry> entryList = queryresult.getResultObjects();
+						if (queryresult.getResultObjects().size() > 0) {
+							for (Entry entry : entryList) {
+								JSONObject response = entry.toJSON();
+								JSONObject url = response.optJSONObject("image");
+								String imageUrl = url.optString("url");
+								allProducts.add(new NewsModel(response.optString("title"),
+										response.optString("description"),
+										imageUrl));
+							}
+						}
+					}
+				}
+			});
+			return allProducts;
+		} catch (Exception e) {
+			System.out.println(e.getMessage());
+			e.printStackTrace();
+		}
+		return null;
+	}
+
+
+	private String getAboutUs() {
+
+		try {
+			final String[] ABOUT_US = {""};
+			Stack stack = Contentstack.stack("blt7979d15c28261b93", "cs17465ae5683299db9d259cb6", "production");
+			Query query = stack.contentType("about").query();
+			query.includeContentType();
+			query.find(new QueryResultsCallBack() {
+
+				@Override
+				public void onCompletion(ResponseType responseType, QueryResult queryresult, Error error) {
+					if (error == null) {
+						ABOUT_US[0] = queryresult.getResultObjects().get(0).toJSON().optString("about");
+					}
+				}
+			});
+			return ABOUT_US[0];
+		} catch (Exception e) {
+			System.out.println(e.getMessage());
+			e.printStackTrace();
+		}
+		return null;
+	}
+
+
+
+	private String getContactUs() {
+
+		try {
+			final String[] contact = {""};
+			Stack stack = Contentstack.stack("blt7979d15c28261b93", "cs17465ae5683299db9d259cb6", "production");
+			Query query = stack.contentType("contactus").query();
+			query.includeContentType();
+			query.find(new QueryResultsCallBack() {
+
+				@Override
+				public void onCompletion(ResponseType responseType, QueryResult queryresult, Error error) {
+					if (error == null) {
+						contact[0] = queryresult.getResultObjects().get(0).toJSON().optString("contactus");
+					}
+				}
+			});
+			return contact[0];
+		} catch (Exception e) {
+			System.out.println(e.getMessage());
+			e.printStackTrace();
+		}
+		return null;
+	}
+}
